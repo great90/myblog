485cc2bbdeb391906bbad831448b032d13db5b31
great <897346026@qq.com>
2015-08-02 12:00:10 +0800

    第一个视图

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-"""myblog URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('blog.urls', namespace='blog')),
-    url(r'^blog/', include('blog.urls', namespace='blog')),
+    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),               
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext, loader
+from django.views import generic
+from django.utils import timezone
 
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'title'
+    
+    def get_queryset(self):
+        """ return the title """
+        return u'%s' % ('index')
+    
 # Create your views here.
